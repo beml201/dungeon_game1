81a21de37fcc3660cbf8b874703f85543fac1653
beml201 <75611311+beml201@users.noreply.github.com>
2024-10-19 12:54:44 +0100

    Updated dungeon generation

--- scripts/dungeon.gd
+++ scripts/dungeon.gd
@@ -1,67 +1,57 @@
 extends Node2D
 
+const TILE_SIZE := 64
 const N_ROOMS := 5
-const ROOM_SIZE := Vector2i(10,14)
+const ROOM_SIZE := Vector2i(6,6)
+const CORRIDOR_LENGTH = 5
 const DIRECTIONS := [Vector2i(1,0),
 					 Vector2i(-1,0),
 					 Vector2i(0, 1),
 					 Vector2i(0, -1)]
+const TILE_DICT := {"ul":Vector2i(0,0), "ml":Vector2i(0,1), "bl":Vector2i(0,2),
+					"um":Vector2i(1,0), "mm":Vector2i(1,1), "bm":Vector2i(1,2),
+					"ur":Vector2i(2,0), "mr":Vector2i(2,1), "br":Vector2i(2,2)}
 
-@onready var tiles: TileMap = $TileMapLayer
+@onready var tiles: TileMapLayer = $RoomMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	generate_room_order()
-	tile_room()
+	print(generate_room_order())
+	draw_room(Vector2i(0,0), ROOM_SIZE[0], ROOM_SIZE[1])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
-func tile_room():
-	var tile_dict := {
-		"CORNER_TOP_LEFT":Vector2i(0,0),
-		"SIDE_LEFT":Vector2i(0,1),
-		"CORNER_BOTTOM_LEFT":Vector2i(0,2),
-		"CORNER_TOP_RIGHT":Vector2i(2,0),
-		"SIDE_RIGHT":Vector2i(2,1),
-		"CORNER_BOTTOM_RIGHT":Vector2i(2,2),
-		"TOP":Vector2i(1,0),
-		"MIDDLE":Vector2i(1,1),
-		"BOTTOM":Vector2i(1,2)
-	}
-	
+func draw_room(coord_upperleft, width, height):
 	var cells := []
-	# Add left side
-	tiles.set_cell(0, Vector2i(0,0), 2, tile_dict["CORNER_TOP_LEFT"])
-	tiles.set_cell(0, Vector2i(0, ROOM_SIZE[1]), 2, tile_dict["CORNER_BOTTOM_LEFT"])
-	for y in range(ROOM_SIZE[1]-1):
-		tiles.set_cell(0, Vector2i(0, y+1), 2, tile_dict["SIDE_LEFT"])
+	for y in range(height):
+		for x in range(width):
+			cells.append(Vector2i(x+coord_upperleft[0], y+coord_upperleft[1]))
+
+	# Generate the tile look
+	var cell_tiles := []
+	cell_tiles.append(TILE_DICT['ul'])
+	for x in range(width-2):
+		cell_tiles.append(TILE_DICT['um'])
+	cell_tiles.append(TILE_DICT['ur'])
+	for y in range(height-2):
+		cell_tiles.append(TILE_DICT['ml'])
+		for x in range(width-2):
+			cell_tiles.append(TILE_DICT['mm'])
+		cell_tiles.append(TILE_DICT['mr'])
+	cell_tiles.append(TILE_DICT['bl'])
+	for x in range(width-2):
+		cell_tiles.append(TILE_DICT['bm'])
+	cell_tiles.append(TILE_DICT['br'])
 	
-	# Add right side
-	tiles.set_cell(0, Vector2i(ROOM_SIZE[0],0), 2, tile_dict["CORNER_TOP_RIGHT"])
-	tiles.set_cell(0, Vector2i(ROOM_SIZE[0], ROOM_SIZE[1]), 2, tile_dict["CORNER_BOTTOM_RIGHT"])
-	for y in range(ROOM_SIZE[1]-1):
-		tiles.set_cell(0, Vector2i(ROOM_SIZE[0], y+1), 2, tile_dict["SIDE_RIGHT"])
-		
-	# Add top
-	for x in range(ROOM_SIZE[0]-1):
-		tiles.set_cell(0, Vector2i(x+1, 0), 2, tile_dict["TOP"])
-	for x in range(ROOM_SIZE[0]-1):
-		tiles.set_cell(0, Vector2i(x+1, ROOM_SIZE[1]), 2, tile_dict["BOTTOM"])
-	for x in range(ROOM_SIZE[0]-1):
-		for y in range(ROOM_SIZE[1]-1):
-			tiles.set_cell(0, Vector2i(x+1,y+1), 2, tile_dict["MIDDLE"])
+	# Print all the tiles
+	for i in range(cells.size()):
+		tiles.set_cell(cells[i], 1, cell_tiles[i])
 	
-	#cells := []	
-	#for y in range(ROOM_SIZE[0]):
-#		for x in range(ROOM_SIZE[1]):
-#			cells.append(Vector2i(x, y))
-#	tiles.set_cells(0, cells, 2, )
-	
-func generate_room_order():
-	var init = Vector2i(0,0)
-	var rooms = [init]
+func generate_room_order() -> Array:
+	var init := Vector2i(0,0)
+	var rooms := [init]
 	for i in range(N_ROOMS):
 		var new_room = rooms.pick_random()
 		var new_room_direction = DIRECTIONS[randi()%4]
@@ -71,4 +61,4 @@ func generate_room_order():
 			new_room_direction = DIRECTIONS[randi()%4]
 			new_room += new_room_direction
 		rooms.append(new_room)
-	print(rooms)
+	return rooms
