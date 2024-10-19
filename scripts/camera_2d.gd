ee8197afa194ff9afebc3604d5591d50c86608ab
beml201 <75611311+beml201@users.noreply.github.com>
2024-10-19 17:12:11 +0100

    Updated corridor generation

---
+++ scripts/camera_2d.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+var zoom_speed = 0.1
+
+func _ready():
+	# Zoom so we can't see other items
+	zoom = Vector2(2, 2)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				zoom = Vector2(0.5, 0.5)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/dungeon.gd
+++ scripts/dungeon.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 const TILE_SIZE := 64
-const N_ROOMS := 5
-const ROOM_SIZE := Vector2i(6,6)
+const N_ROOMS := 6
+const ROOM_SIZE := Vector2i(10,10)
 const CORRIDOR_LENGTH = 5
 const DIRECTIONS := [Vector2i(1,0),
 					 Vector2i(-1,0),
@@ -13,11 +13,20 @@ const TILE_DICT := {"ul":Vector2i(0,0), "ml":Vector2i(0,1), "bl":Vector2i(0,2),
 					"ur":Vector2i(2,0), "mr":Vector2i(2,1), "br":Vector2i(2,2)}
 
 @onready var tiles: TileMapLayer = $RoomMap
+var generated_rooms: Array = []
+var room_pos_ul: Array = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(generate_room_order())
-	draw_room(Vector2i(0,0), ROOM_SIZE[0], ROOM_SIZE[1])
+	generated_rooms = generate_room_order()
+	print(generated_rooms)
+	for room in generated_rooms[0]:
+		var room_ul := Vector2i((room[0]*ROOM_SIZE[0]),
+								(room[1]*ROOM_SIZE[1]))
+		room_pos_ul.append(room_ul)
+		draw_room(room_ul, ROOM_SIZE[0], ROOM_SIZE[1])
+	for i in range(1, N_ROOMS):
+		generate_corridor(i)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -52,7 +61,8 @@ func draw_room(coord_upperleft, width, height):
 func generate_room_order() -> Array:
 	var init := Vector2i(0,0)
 	var rooms := [init]
-	for i in range(N_ROOMS):
+	var direction_history := [init]
+	for i in range(N_ROOMS-1):
 		var new_room = rooms.pick_random()
 		var new_room_direction = DIRECTIONS[randi()%4]
 		new_room += new_room_direction
@@ -61,4 +71,19 @@ func generate_room_order() -> Array:
 			new_room_direction = DIRECTIONS[randi()%4]
 			new_room += new_room_direction
 		rooms.append(new_room)
-	return rooms
+		direction_history.append(new_room_direction)
+	return [rooms, direction_history]
+	
+func generate_corridor(n_events):
+	# Create corridor from new room to old room
+	var which_side = generated_rooms[1][n_events]*Vector2i(-1,-1)
+	# Navigate the corridor to the right position
+	var corridor_pos = (which_side + Vector2i(1,1))
+	# Standardise to world
+	corridor_pos = room_pos_ul[n_events] + corridor_pos*(ROOM_SIZE+Vector2i(-1,-1))/2
+	# Add the other end
+	var cp2 = corridor_pos+Vector2i(which_side)
+	
+	tiles.set_cell(corridor_pos, 1, TILE_DICT['mm'])
+	tiles.set_cell(cp2, 1, TILE_DICT['mm'])
+	
